21a1ed3ae06bb0d11c80c2a1d318bc55b8c56d60
aqyuki <aqyuki@aqyuki.dev>
2024-11-08 21:41:50 +0900

    feat: implement shutdown

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 
 	"github.com/TechBowl-japan/go-stations/db"
@@ -67,7 +71,36 @@ func realMain() error {
 	mux := middleware.Recovery(middleware.BoxOSInfo(middleware.Logging(router.NewRouter(todoDB, userID, password))))
 
 	// TODO: サーバーをlistenする
-	if err := http.ListenAndServe(defaultPort, mux); err != nil {
+
+	server := http.Server{
+		Addr:    port,
+		Handler: mux,
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		ctx, done := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+		defer done()
+
+		<-ctx.Done()
+		log.Println("main: signal received and shutting down server")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("main: failed to shutdown server, err =%v\n", err)
+		}
+		log.Println("main: server shutdown successfully")
+	}()
+
+	err = server.ListenAndServe()
+	wg.Wait()
+	if err != nil {
 		if errors.Is(err, http.ErrServerClosed) {
 			return nil
 		}
