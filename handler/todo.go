0ca7283f2277294a8e594e685992fecd1791e961
aqyuki <aqyuki@aqyuki.dev>
2024-11-08 19:08:01 +0900

    feat: implement UpdateTODO handler

--- handler/todo.go
+++ handler/todo.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/model"
@@ -39,8 +40,14 @@ func (h *TODOHandler) Read(ctx context.Context, req *model.ReadTODORequest) (*mo
 
 // Update handles the endpoint that updates the TODO.
 func (h *TODOHandler) Update(ctx context.Context, req *model.UpdateTODORequest) (*model.UpdateTODOResponse, error) {
-	_, _ = h.svc.UpdateTODO(ctx, 0, "", "")
-	return &model.UpdateTODOResponse{}, nil
+
+	task, err := h.svc.UpdateTODO(ctx, req.ID, req.Subject, req.Description)
+	if err != nil {
+		return nil, err
+	}
+	return &model.UpdateTODOResponse{
+		TODO: task,
+	}, nil
 }
 
 // Delete handles the endpoint that deletes the TODOs.
@@ -53,24 +60,62 @@ func (h *TODOHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case http.MethodPost:
-		var req model.CreateTODORequest
-		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		if req.Subject == "" {
-			http.Error(w, "subject is required", http.StatusBadRequest)
-			return
-		}
+		h.handleCreate(w, r)
+	case http.MethodPut:
+		h.handleUpdate(w, r)
+	}
+}
 
-		todo, err := h.Create(r.Context(), &req)
-		if err != nil {
+func (h *TODOHandler) handleCreate(w http.ResponseWriter, r *http.Request) {
+	var req model.CreateTODORequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if req.Subject == "" {
+		http.Error(w, "subject is required", http.StatusBadRequest)
+		return
+	}
+
+	todo, err := h.Create(r.Context(), &req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := json.NewEncoder(w).Encode(&todo); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *TODOHandler) handleUpdate(w http.ResponseWriter, r *http.Request) {
+	var req model.UpdateTODORequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if req.ID == 0 {
+		http.Error(w, "id is disallowed to be 0", http.StatusBadRequest)
+		return
+	}
+	if req.Subject == "" {
+		http.Error(w, "subject is required", http.StatusBadRequest)
+		return
+	}
+
+	todo, err := h.Update(r.Context(), &req)
+	if err != nil {
+		if errors.Is(err, model.ErrNotFound{}) {
+			http.Error(w, err.Error(), http.StatusNotFound)
+		} else {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
 		}
-		if err := json.NewEncoder(w).Encode(&todo); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(&todo); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
