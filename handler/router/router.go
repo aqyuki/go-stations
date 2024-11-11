1c6e67f3f45672a7c6b16a4c55986983cb26b04f
aqyuki <aqyuki@aqyuki.dev>
2024-11-11 17:19:40 +0900

    Squash merge feat/latter-half-task into main

--- handler/router/router.go
+++ handler/router/router.go
@@ -5,14 +5,16 @@ import (
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/handler"
+	"github.com/TechBowl-japan/go-stations/handler/middleware"
 	"github.com/TechBowl-japan/go-stations/service"
 )
 
-func NewRouter(todoDB *sql.DB) *http.ServeMux {
+func NewRouter(todoDB *sql.DB, userID, password string) *http.ServeMux {
 	// register routes
 	mux := http.NewServeMux()
 	mux.Handle("/healthz", handler.NewHealthzHandler())
 	mux.Handle("/do-panic", &handler.DoPanicHandler{})
 	mux.Handle("/todos", handler.NewTODOHandler(service.NewTODOService(todoDB)))
+	mux.Handle("/auth", middleware.NewBasicAuthMiddleware(userID, password)(handler.NewAuthHandler()))
 	return mux
 }
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 
 	"github.com/TechBowl-japan/go-stations/db"
@@ -22,8 +26,10 @@ func main() {
 func realMain() error {
 	// config values
 	const (
-		defaultPort   = ":8080"
-		defaultDBPath = ".sqlite3/todo.db"
+		defaultPort     = ":8080"
+		defaultDBPath   = ".sqlite3/todo.db"
+		defaultUserID   = "admin"
+		defaultPassword = "password"
 	)
 
 	port := os.Getenv("PORT")
@@ -34,6 +40,16 @@ func realMain() error {
 	dbPath := os.Getenv("DB_PATH")
 	if dbPath == "" {
 		dbPath = defaultDBPath
+	}
+
+	userID := os.Getenv("BASIC_AUTH_USER_ID")
+	if userID == "" {
+		userID = defaultUserID
+	}
+
+	password := os.Getenv("BASIC_AUTH_PASSWORD")
+	if password == "" {
+		password = defaultPassword
 	}
 
 	// set time zone
@@ -52,10 +68,39 @@ func realMain() error {
 
 	// NOTE: 新しいエンドポイントの登録はrouter.NewRouterの内部で行うようにする
 	// 呼び出し順は、Recovery -> BoxOSInfo -> Logging(before) -> handler -> Logging(after) -> Recovery(defer)
-	mux := middleware.Recovery(middleware.BoxOSInfo(middleware.Logging((router.NewRouter(todoDB)))))
+	mux := middleware.Recovery(middleware.BoxOSInfo(middleware.Logging(router.NewRouter(todoDB, userID, password))))
 
 	// TODO: サーバーをlistenする
-	if err := http.ListenAndServe(defaultPort, mux); err != nil {
+
+	server := http.Server{
+		Addr:    port,
+		Handler: mux,
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		ctx, done := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+		defer done()
+
+		<-ctx.Done()
+		log.Println("main: signal received and shutting down server")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("main: failed to shutdown server, err =%v\n", err)
+		}
+		log.Println("main: server shutdown successfully")
+	}()
+
+	err = server.ListenAndServe()
+	wg.Wait()
+	if err != nil {
 		if errors.Is(err, http.ErrServerClosed) {
 			return nil
 		}
